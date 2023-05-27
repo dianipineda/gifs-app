06622c4436f804bd33896469b271449bb21273c3
dianipineda <dianipineda55@gmail.com>
2023-05-27 09:33:09 -0500

    terminación de la seccion 8 y la app gifs

--- src/app/shared/lazyimage/lazyimage.component.ts
+++ src/app/shared/lazyimage/lazyimage.component.ts
@@ -1,11 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'shared-lazy-image',
   templateUrl: './lazyimage.component.html',
 })
 export class LazyimageComponent implements OnInit {
-  constructor() {}
+  @Input()
+  public url!: string;
+  @Input()
+  public alt!: string;
 
-  ngOnInit() {}
+  public hasLoaded: boolean = false;
+
+  ngOnInit(): void {
+    if (!this.url) throw new Error('la url no es proporcionada');
+  }
+
+  onLoad() {
+    setTimeout(() => {
+      this.hasLoaded = true;
+      console.log('Image loaded');
+    }, 1000);
+  }
 }
